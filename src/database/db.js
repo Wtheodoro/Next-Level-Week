180e260a8daff275d1c131789843c710c7c01ca2
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-08-09 21:53:01 -0300

    UP

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,29 @@ const Database = require('sqlite-async')
 Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db) {
-    console.log('cheguei aqui')
-    //console.log(db)
+    // Create database tables
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS proffys (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            whatsapp TEXT,
+            bio TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS classes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            cost TEXT,
+            proffy_id INTERGER
+        );
+
+        CREATE TABLE IF NOT EXISTS class_schedule (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            class_id INTEGER,
+            weekday INTEGER,
+            time_from INTEGER,
+            time_to INTEGER
+        );
+    `)
 }
